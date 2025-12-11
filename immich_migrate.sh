43c88b962eabf712ec5741bb1c35875dc80351ee
kuna <69559852+Kuna-Labs@users.noreply.github.com>
2025-12-11 13:40:39 -0600

    Update old and new pool paths in migration script

--- immich_migrate.sh
+++ immich_migrate.sh
@@ -7,8 +7,8 @@
 #   preserving media and database files.
 
 # Change these paths for your environment
-OLD_POOL="/mnt/.ix-apps/app_mounts/immich"
-NEW_POOL="/mnt/HDDs/immich"
+OLD_POOL="/mnt/HDDs/Applications/immich"
+NEW_POOL="/mnt/HDDs/Applications/immich"
 
 echo ">>> Creating target directories..."
 mkdir -p ${NEW_POOL}/data/{upload,thumbs,library,profile,backups,encoded-video}
